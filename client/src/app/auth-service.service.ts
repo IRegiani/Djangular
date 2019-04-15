26eeecabedb07c0cf7c9719cff8c07217db5464f
Iago R Regiani <IagoRRegiani@users.noreply.github.com>
2019-04-15 19:49:37 -0300

    Service frame

--- client/src/app/auth-service.service.ts
+++ client/src/app/auth-service.service.ts
@@ -4,10 +4,38 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class AuthServiceService {
+  
+   User = {
+
+  };
 
   constructor() {
 
    }
 
-   
+  authenticateUser(login: string, password: string) {
+    // conectar ao banco
+    // enviar dados de login pela api do django
+    // caso receba ID do usuário, salvar no aqui
+    // Retorna o true
+    // caso erro: "Erro de usuário/senha"
+  } 
+
+  getUserLocalData(){
+    //retorna o objeto local do usuario
+  }
+
+  createNewUser(cadastro: User){
+    // conectar ao banco com post
+    // enviar dados do cadastro para o banco salvar
+    // recebe um true sobre os dados do novo usuario esta salvo
+    // retorna um true
+    // caso erro: "login/email existente"
+
+  }
+
+  verifyUserType() {
+    return 'Admin';
+  }
+
 }
