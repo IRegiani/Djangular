811ddba74d64bf1bd13895bb1949558674d288db
Vítor <v.pochmann15@gmail.com>
2019-05-13 19:43:03 -0300

    update functions of GET, POST, PUT

--- server/djangular/serve/api.py
+++ server/djangular/serve/api.py
@@ -1,33 +1,82 @@
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
+
+from django_filters.rest_framework import DjangoFilterBackend
 
 from .serializers import PessoaSerializer, AdministradorSerializer, CursoSerializer, TurmaSerializer, AulaSerializer, PessoaAulaSerializer, ColaboradorTurmaSerializer
 from .models import Administrador, Pessoa, Curso, Aula, Turma, ColaboradorTurma, PessoaAula
+
+from rest_framework.decorators import api_view
 
 class AdministradorAPI(ListAPIView):
     queryset = Administrador.objects.all()
     serializer_class = AdministradorSerializer
 
-class PessoaAPI(ListAPIView):
+
+class PessoaAPI(ModelViewSet):
     queryset = Pessoa.objects.all()
     serializer_class = PessoaSerializer
-    
+    print("FUNCIONA!!!")
+
+#class de GET detalhe da Pessoa
+class DetalhePessoaAPI(ListAPIView):
+    serializer_class = PessoaSerializer
+    def get_queryset(self):
+        idpessoa = self.kwargs['id']
+        print("Id Pessoa = ", idpessoa)
+        queryset = Pessoa.objects.filter(id=idpessoa)
+        return queryset
+
 class CursoAPI(ListAPIView):
     queryset = Curso.objects.all()
     serializer_class = CursoSerializer
+
+
+#class de GET detalhe do Curso
+class DetalheCursoAPI(ListAPIView):
+    serializer_class = CursoSerializer
     
+    def get_queryset(self):
+        idcurso = self.kwargs['id']
+        queryset = Curso.objects.filter(id=idcurso)
+        return queryset
+
 class TurmaAPI(ListAPIView):
     queryset = Turma.objects.all()
     serializer_class = TurmaSerializer
-    
+
+
 class AulaAPI(ListAPIView):
     queryset = Aula.objects.all()
     serializer_class = AulaSerializer
+
 
 class ColaboradorTurmaAPI(ListAPIView):
     queryset = ColaboradorTurma.objects.all()
     serializer_class = ColaboradorTurmaSerializer
 
+
 class PessoaAulaAPI(ListAPIView):
     queryset = PessoaAula.objects.all()
     serializer_class = PessoaAulaSerializer
-   
+
+
+#class POST detalhe para adicionar aluno X na aula Y
+class AddPessoaAulaAPI(ListAPIView):
+    serializer_class = PessoaAulaSerializer
+    serializerPessoa = PessoaSerializer
+    serializerAula = AulaSerializer
+
+    @api_view(['GET', 'PUT'])
+    def get_queryset(self, request):
+        if request.method == 'GET':
+            queryset = PessoaAula.objects.all()
+            return queryset
+        if request.method == 'PUT':
+            data = request.data
+            print(data)
+            queryset = data
+            return data
+            
+
--- server/djangular/serve/urls.py
+++ server/djangular/serve/urls.py
@@ -1,12 +1,20 @@
 from django.conf.urls import url
-from .api import AdministradorAPI, PessoaAPI, CursoAPI, TurmaAPI, AulaAPI, PessoaAulaAPI, ColaboradorTurmaAPI
+from .api import AdministradorAPI, PessoaAPI, CursoAPI, TurmaAPI, AulaAPI, PessoaAulaAPI, ColaboradorTurmaAPI, DetalhePessoaAPI, DetalheCursoAPI, AddPessoaAulaAPI
+
+from rest_framework.routers import DefaultRouter
+
+
 
 urlpatterns = [
     url(r'^adm$', AdministradorAPI.as_view()),
-    url(r'^pessoas$', PessoaAPI.as_view()),
+    url(r'^pessoas/$', PessoaAPI.as_view({'get': 'list', 'post': 'create'})),
+    url(r'^pessoas/(?P<pk>.+)/', PessoaAPI.as_view({'get': 'retrieve', 'put': 'update'})),
+    url(r'^detalhePessoa/(?P<id>.+)$', DetalhePessoaAPI.as_view()),
     url(r'^cursos$', CursoAPI.as_view()),
+    url(r'^detalheCurso/(?P<id>.+)$', DetalheCursoAPI.as_view()),
     url(r'^turmas$', TurmaAPI.as_view()),
     url(r'^aulas$', AulaAPI.as_view()),
     url(r'^pa$', PessoaAulaAPI.as_view()),
     url(r'^ct$', ColaboradorTurmaAPI.as_view()),
+    url(r'^addPA$', AddPessoaAulaAPI.as_view()),
 ]
