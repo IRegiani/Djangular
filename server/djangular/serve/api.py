68f615de1d68a5d326c335361975f42a2c299af2
Vítor <v.pochmann15@gmail.com>
2019-06-01 00:26:37 -0300

    alter API
    modificação de todos os API com django_filters

--- server/djangular/serve/api.py
+++ server/djangular/serve/api.py
@@ -1,33 +1,82 @@
 from rest_framework.generics import ListAPIView
+from rest_framework import viewsets
+from django_filters.rest_framework import DjangoFilterBackend
 
-from .serializers import PessoaSerializer, AdministradorSerializer, CursoSerializer, TurmaSerializer, AulaSerializer, PessoaAulaSerializer, ColaboradorTurmaSerializer
-from .models import Administrador, Pessoa, Curso, Aula, Turma, ColaboradorTurma, PessoaAula
+from .serializers import *
+from .models import *
 
-class AdministradorAPI(ListAPIView):
+## API de Serializers Normais ###############
+
+class AdministradorAPI(viewsets.ModelViewSet):
     queryset = Administrador.objects.all()
     serializer_class = AdministradorSerializer
-
-class PessoaAPI(ListAPIView):
-    queryset = Pessoa.objects.all()
-    serializer_class = PessoaSerializer
     
-class CursoAPI(ListAPIView):
+class PessoaAPI(viewsets.ModelViewSet):
+    queryset = Pessoa.objects.all()
+    filter_backends = (DjangoFilterBackend,)
+    filterset_fields = ('Name',)
+
+    def get_serializer_class(self):
+        metodo = self.request.method
+        if metodo == 'PUT' or metodo == 'POST':
+            return PessoaSerializer
+        else:
+            return GetPessoaSerializer
+
+
+class CursoAPI(viewsets.ModelViewSet):
     queryset = Curso.objects.all()
+    filter_backends = (DjangoFilterBackend,)
+    filterset_fields = ('Name',)
     serializer_class = CursoSerializer
     
-class TurmaAPI(ListAPIView):
+
+class TurmaAPI(viewsets.ModelViewSet):
     queryset = Turma.objects.all()
-    serializer_class = TurmaSerializer
+    filter_backends = (DjangoFilterBackend,)
+    filterset_fields = ('Name',)
+
+    def get_serializer_class(self):
+        metodo = self.request.method
+        if metodo == 'PUT' or metodo == 'POST':
+            return TurmaSerializer
+        else:
+            return GetTurmaSerializer
     
-class AulaAPI(ListAPIView):
+class AulaAPI(viewsets.ModelViewSet):
     queryset = Aula.objects.all()
-    serializer_class = AulaSerializer
+    filter_backends = (DjangoFilterBackend,)
+    filterset_fields = ('Assunto',)
 
-class ColaboradorTurmaAPI(ListAPIView):
+    def get_serializer_class(self):
+        metodo = self.request.method
+        if metodo == 'PUT' or metodo == 'POST':
+            return AulaSerializer
+        else:
+            return GetAulaSerializer
+
+## API de Serializers Relacionais ###############
+
+class ColaboradorTurmaAPI(viewsets.ModelViewSet):
     queryset = ColaboradorTurma.objects.all()
-    serializer_class = ColaboradorTurmaSerializer
+    filter_backends = (DjangoFilterBackend,)
+    filterset_fields = ('Description',)
 
-class PessoaAulaAPI(ListAPIView):
+    def get_serializer_class(self):
+        metodo = self.request.method
+        if metodo == 'PUT' or metodo == 'POST':
+            return ColaboradorTurmaSerializer
+        else:
+            return GetColaboradorTurmaSerializer
+
+class PessoaAulaAPI(viewsets.ModelViewSet):
     queryset = PessoaAula.objects.all()
-    serializer_class = PessoaAulaSerializer
-   
+    filter_backends = (DjangoFilterBackend,)
+    filterset_fields = ('id',)
+
+    def get_serializer_class(self):
+        metodo = self.request.method
+        if metodo == 'PUT' or metodo == 'POST':
+            return PessoaAulaSerializer
+        else:
+            return GetPessoaAulaSerializer
