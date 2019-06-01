321a9157b7eb7d75e162cb6cc92f01c6f591d91e
Vítor <v.pochmann15@gmail.com>
2019-06-01 00:28:29 -0300

    Alter url
    Alteração do API com django_filters e ModelViewSet realizado; Criação de novos Serializers , como GetSerializers em alguns modelos.

--- server/djangular/serve/urls.py
+++ server/djangular/serve/urls.py
@@ -1,6 +1,11 @@
 from django.conf.urls import url
-from .api import AdministradorAPI, PessoaAPI, CursoAPI, TurmaAPI, AulaAPI, PessoaAulaAPI, ColaboradorTurmaAPI
+from .api import *
 
+
+# TODO: precisa corrigir os url para que o Django possa usar o ModelViewSet corretamente
+# Na compilação, o django reclama que precisa argumentar os 'as_view()' como:
+#   API.as_view({'get': 'lists'})
+# Porem, se lembro bem, o Pedro Cotta fez de um jeito que não precisava argumentar os 'as_view()'
 urlpatterns = [
     url(r'^adm$', AdministradorAPI.as_view()),
     url(r'^pessoas$', PessoaAPI.as_view()),
